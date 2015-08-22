a0192daddd96f95e97e4851fe38546393e1e72ff
ChrisSensebe <christophe.sensebe@gmail.com>
2015-08-22 20:03:19 +0200

    coding logKey function & db connection

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var mongodb     = require('mongodb'),
-	fs          = require('fs');
+	fs          = require('fs'),
+	MongoClient = mongodb.MongoClient;
 
-fs.readFile('allSets.json', function(err, data){
+
+MongoClient.connect("mongodb://localhost:27017/hearthstone", function(err, db){
 	if(err){
 		return callback(err);
 	}
-	var sets = JSON.parse(data);
+	fs.readFile('allSets.json', logKeys);
 });
+
+function logKeys(err, data){
+	if(err){
+		return callback(err);
+	}
+	var keys = Object.keys(JSON.parse(data));
+	console.log(keys);
+}
