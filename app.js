8c87a4d1bc89ce1b68fba2061506f34c9e003b9c
ChrisSensebe <christophe.sensebe@gmail.com>
2015-08-22 23:26:17 +0200

    adding function to save data in db

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ var mongodb     = require('mongodb'),
 	fs          = require('fs'),
 	MongoClient = mongodb.MongoClient;
 
-
 MongoClient.connect("mongodb://localhost:27017/hearthstone", function(err, db){
-	if(err){
-		return callback(err);
-	}
-	fs.readFile('allSets.json', logKeys);
-});
 
-function logKeys(err, data){
 	if(err){
-		return callback(err);
+		return console.error(err);
 	}
-	var keys = Object.keys(JSON.parse(data));
-	console.log(keys);
-}
+	fs.readFile('ALLSets.json', function(err, data){
+		if(err){
+			return console.error(err);
+		}
+		db.collection('allSets').save(JSON.parse(data), function(err, doc){
+			if(err){
+				return console.error(err);
+			}
+			console.log('document inserted');
+			db.close();
+		});
+	});
+});
