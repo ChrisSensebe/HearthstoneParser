e73a67fde01cc7881b72e9a6b6c9334ea6c6c1c0
ChrisSensebe <christophe.sensebe@gmail.com>
2015-08-23 21:59:39 +0200

    adding set poperty for each card in collection

--- app.js
+++ app.js
@@ -2,27 +2,34 @@ var mongodb     = require('mongodb'),
 	fs          = require('fs'),
 	MongoClient = mongodb.MongoClient;
 
+// Connects to db
 MongoClient.connect("mongodb://localhost:27017/hearthstone", function(err, db){
 
 	if(err){
 		console.error(err);
 	}
+	// Gets data from file ALLSets.json -> {collection : [{card},{card}...], collection : [{card},{card}...], ....}
 	fs.readFile('ALLSets.json', function(err, data){
 		if(err){
 			console.error(err);
 		}
 		var allSets  = JSON.parse(data);
 		var allCards = [];
+		// Populates allCards with card data -> [{card}, {card}, ...]
 		for(var property in allSets){
+			// Adds set name for each card
+			for(var i=0; i<allSets[property].length; i++){
+				allSets[property][i].set = property;
+			}
 			allCards = allCards.concat(allSets[property]);
 		}
-
+		// Saves all cards in db
 		db.collection('allSets').insert(allCards, function(err, result){
 			if(err){
-				console.error(err);
+				console.error('error :' + err);
 			};
-			console.log('Document inserted');
+			console.log('Cards saved in db');
+			db.close();
 		});
-		db.close();
 	});
 });
